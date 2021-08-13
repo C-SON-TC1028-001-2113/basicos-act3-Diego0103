f9cc9cf4d2072f9c04de14dbf3f9a2dedd18d4a0
Diego0103 <A01254073@itesm.mx>
2021-08-13 18:07:07 +0000

    eJERCICIO 2

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
-
+    a=int(input('Dame un número: '))
+    b=int(input('Dame un número: '))
+    suma = a+b
+    resta = a-b
+    mult = a*b
+    print ("Suma: "+str(suma))
+    print ("Resta: "+str(resta))
+    print ("Multiplicación: "+str(mult))
 if __name__ == '__main__':
     main()
